2aa7b352a0195dec74fdba02b858da045588f703
Harry2687 <harry.zhong2687@gmail.com>
2024-03-23 14:27:19 +0800

    Completed Spotify Clustering post

--- projects/spotify-analysis/scripts/generate_token.R
+++
@@ -1,9 +0,0 @@
-spotify_token <- POST(
-  "https://accounts.spotify.com/api/token",
-  authenticate("983661a33f7e477eb72effaf65d556b5",
-               "a9cca562c7eb451ca9173d45bd5564f9"),
-  body = list(grant_type = "client_credentials"),
-  encode = "form"
-) %>%
-  content() %>%
-  pluck(1)
